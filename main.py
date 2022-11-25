cf9e81d2343cb9cf6911a5fa9080c5ea3c14a972
akhmedov04 <119047485+akhmedov04@users.noreply.github.com>
2022-11-25 21:52:04 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import os
 from aiogram import *
 from pytube import YouTube
 
-bot = Bot("5897647826:AAEQxdfFgV45NW20TMlrF-l3f6rLJpgWTGk")
+bot = Bot("5885229538:AAErgF3u22W9B8ohN-UmUQ--Eigdg84jQGc")
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=["start"])
 async def start_message(message:types.Message):
     chat_id = message.chat.id
-    await bot.send_message(chat_id , "Salom, bu bot Youtube dan video yuklaydi\n"
-                                     "Linkni jonating")
+    await bot.send_message(chat_id , "Привет, я могу скачать видео с YouTube\n"
+                                     "Отправьте мне ссылку!")
 
 
 @dp.message_handler()
@@ -19,7 +19,7 @@ async def text_message(message:types.Message):
     url = message.text
     yt = YouTube(url)
     if message.text.startswith == "https://youtu.be/" or "https://youtube.com/" :
-        await bot.send_message(chat_id, "Biroz kuting, yuklanmoqda!")
+        await bot.send_message(chat_id, "Начинаю загрузку видео!")
         await download_youtube_video(url, message, bot)
     else:
         await bot.send_message(chat_id, ":(")
@@ -30,7 +30,7 @@ async def download_youtube_video(url, message, bot):
     stream = yt.streams.filter(progressive=True, file_extension="mp4")
     stream.get_highest_resolution().download(f"{message.chat.id}", f"{message.chat.id}_{yt.title}")
     with open(f"{message.chat.id}/{message.chat.id}_{yt.title}", "rb") as video:
-        await bot.send_video(message.chat.id, video, caption="*Tayyor!*", parse_mode="Markdown")
+        await bot.send_video(message.chat.id, video, caption="*Готово!*", parse_mode="Markdown")
         os.remove(f"{message.chat.id}/{message.chat.id}_{yt.title}")
 
 
